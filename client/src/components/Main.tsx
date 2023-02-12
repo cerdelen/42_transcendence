e57f7568d228daeec6acf6355794e172ceefca4b
kmilchev <kmilchev@student.42wolfsburg.de>
2023-02-13 00:25:14 +0100

    stylistic changes

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,9 +1,8 @@
 import {IoLogoGameControllerA} from 'react-icons/io'
 import {MdOutlineVideogameAssetOff} from 'react-icons/md'
 import {players} from './temp-players'
+import { useState } from 'react'
 
-import catYellowImg from '../images/cat-yellow.jpg'
-import catLyingImg from '../images/cat-lying.jpg'
 interface PlayerProps {
 	username: string;
 	photo: string;
@@ -43,15 +42,42 @@ const ListPlayersOnline = () => {
 	)
 }
 
-const GameOnlineCard = () => {
+interface LiveGameProps {
+	username1: string;
+	photo1: string;
+	username2: string;
+	photo2: string;
+}
+const GameOnlineCard = ({username1, photo1, username2, photo2}: LiveGameProps) => {
+	
+	const [showText, setShowText] = useState(false);
+
+	const handleMouseOver = () => {
+	  setShowText(true);
+	};
+  
+	const handleMouseOut = () => {
+	  setShowText(false);
+	};
+
+	const handleOnClick = () => {
+		alert("A message to ask if you are sure you want to leave the current screen to go watch the game")
+	}
+	
 	return (
-		<li className='game-card-li'>
+		<li className='game-card-li' onClick={handleOnClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+			<div>
+				<img  src={photo1} alt="" />
+				{showText && <span className='game-card-user-name'>{username1}</span>}
+			</div>
+			
+			{!showText ? <span>VS</span> : <span className="vertical-word">Watch</span> }
 
-			<img src={catLyingImg} alt="" />
-			<span>VS</span>
-			<img src={catYellowImg} alt="" />
-
-
+			<div>
+				<img src={photo2} alt=""/>
+				{showText && <span className='game-card-user-name'>{username2}</span>}
+			</div>
+			
 		</li>
 	)
 }
@@ -60,7 +86,12 @@ const ListLiveGames = () => {
 	return (
 		<ul className='game-page-games-online-ul'>
 			{players.map((player, idx) => (
-				<GameOnlineCard/>
+				<GameOnlineCard
+					username1={players[9].name}
+					photo1={players[9].photo}
+					username2={players[5].name}
+					photo2={players[5].photo}
+				/>
 			))}
 		</ul>
 	)
--- client/src/components/temp-players.tsx
+++ client/src/components/temp-players.tsx
@@ -3,6 +3,7 @@ import catSternImg from '../images/cat-stern.jpeg'
 import catGrassImg from '../images/cat-grass.jpg'
 import catYellowImg from '../images/cat-yellow.jpg'
 import catLyingImg from '../images/cat-lying.jpg'
+import catWhiteImg from '../images/cat-white.jpg'
 
 class Player {
 
@@ -27,8 +28,8 @@ players.push(new Player('Jakub', catSternImg, false));
 players.push(new Player('Wolfito', catGrassImg, false));
 players.push(new Player('Zoe', catYellowImg, true));
 players.push(new Player('Krisi', catLyingImg, true));
-players.push(new Player('Jakuldfjhvjhgdsfjghdfsjghdsvgddgvdlsvlsllhgvlgsdfb', catSternImg, false));
+players.push(new Player('Jakuldfjhvjhgdsfjghdfsjghds vgddgvdlsvl sllhgvlgsdfb', catSternImg, false));
 players.push(new Player('Cedric', catFlowersImg, true));
 players.push(new Player('Jakub', catSternImg, false));
 players.push(new Player('Cedric', catFlowersImg, true));
-players.push(new Player('Jakub', catSternImg, false));
+players.push(new Player('Kristiyana', catWhiteImg, false));

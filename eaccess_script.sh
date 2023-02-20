42f734cdd2bf6682b9dcaa7a17c86cbbc826a072
Cedric Erdelen <cerdelen@c1s7d33.42wolfsburg.de>
2023-02-20 13:56:51 +0100

    cleaning main branch

--- eaccess_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir ~/.npm-global || true
-npm config set prefix '~/.npm-global' || true
-echo "export PATH=~/.npm-global/bin:$PATH" > ~/.profile || true
-source ~/.profile || true
--- prisma/migrations/20230106151430_fix_typo/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `hashRt` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "hashRt",
-ADD COLUMN     "hashedRt" TEXT;

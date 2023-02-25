bb7293c756ecc55b90a057d10412ca1a96fb735e
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-02-25 17:59:57 +0100

    login working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
@@ -6,6 +6,17 @@ import HomePage from "./components/HomePage";
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
+
+  useEffect(() => {
+    const myCookieValue = document.cookie
+      .split('; ')
+      .find(cookie => cookie.startsWith('accessToken='))
+      ?.split('=')[1];
+    if (myCookieValue !== undefined) {
+      setLoggedIn(true);
+    }
+  }, []);
+  
   return (
     <>
       {loggedIn ? 

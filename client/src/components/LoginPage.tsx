ffb1bf1a86bd2a15123fe67654c6ad0a65968672
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-02-25 15:04:18 +0100

    assign function

--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -10,10 +10,9 @@ const LoginPage = ({loggedIn, setLoggedIn}: Props) => {
 
   const handleLoginClick = async() => {
     try {
-      const response = await fetch('/auth/login', {method: 'GET', credentials: "include"});
-      console.log(response);
-      // window.location.assign("https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-ebe5af0f2962dca5114adf05b60c69a7cbbb6ec31e4cd146812b74d954feb284&redirect_uri=http%3A%2F%2Flocalhost%3A3003%2Fauth%2Flogin&response_type=code");
-      // const data = await response.json();
+      // const response = await fetch('/auth/login', {method: 'GET', credentials: "include"});
+      // console.log(response);
+      window.location.assign("https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-ebe5af0f2962dca5114adf05b60c69a7cbbb6ec31e4cd146812b74d954feb284&redirect_uri=http%3A%2F%2Flocalhost%3A3003%2Fauth%2Flogin&response_type=code");
       // setLoggedIn(true);
       
     }

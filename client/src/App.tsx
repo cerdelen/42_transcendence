66fe7c7172206403e2e9587ae1cecb3bc7678308
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-02-28 21:43:00 +0100

    updated the main one

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,33 +4,34 @@ import "./styles/chat.css"
 import "./styles/open-chat-card.css"
 import "./styles/toggle-button.css"
 import "./styles/qr-form.css"
+import JSCookies from 'js-cookie'
 import "./styles/second-factor-page.css"
 import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
 import { MyProvider } from "./components/AppContext";
 import SecondFactorPage from "./components/SecondFactorPage";
+
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-
-
+  const [isTwoFactor, setTwoFactor] = useState(false);
+  
+  
   useEffect(() => {
-    const myCookieValue = document.cookie
-      .split('; ')
-      .find(cookie => cookie.startsWith('accessToken='))
-      ?.split('=')[1];
-    if (myCookieValue !== undefined || myCookieValue === '') {
+    const urlParams = new URLSearchParams(window.location.search);
+    setTwoFactor(urlParams.has('2-fa'));
+
+    const myCookie = JSCookies.get('accessToken');
+    if (myCookie !== undefined || myCookie === '') {
       setLoggedIn(true);
     }
-    console.log(myCookieValue);
+    // console.log(myCookie);
   }, []);
   return (
-    <MyProvider>
-      {/* <SecondFactorPage/> */}
+    <MyProvider loggedIn={loggedIn} setLoggedIn={setLoggedIn}>
+      {isTwoFactor &&  <SecondFactorPage/>}
       {loggedIn && <HomePage />}
-      
       {!loggedIn && <LoginPage /> }
       
-      {/* {communityPage ? <> } */}
     </MyProvider>
   );
 }

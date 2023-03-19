10bd71685bdfe238f86af10bfe142d04276bfd4b
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-03-19 18:33:03 +0100

    controllers to get games history

--- server/src/game/game.service.ts
+++ server/src/game/game.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 
 @Injectable()
 export class GameService {
+
+  constructor(
+		private readonly prisma: PrismaService,
+	) {}
+
+
+  async get_many_games(game_ids: number[])
+  {
+    let games = [];
+    for (var val of game_ids)
+    {
+      let game;
+      if(!Number.isNaN(val))
+        game = await this.get_one_game(val);
+      if(game)
+      {
+        games.push(game);
+      }
+    }
+    return (games);
+  }
+
+  async get_one_game(game_id: number)
+  {
+    return (this.prisma.game.findUnique({where: {id: game_id}}));
+  }
+
   create(createGameDto: CreateGameDto) {
     return 'This action adds a new game';
   }

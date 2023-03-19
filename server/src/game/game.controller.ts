d4550f5c4b954520a68b4ab9f23f5f9c5953d2a0
Cedric Erdelen <cerdelen@c2s3d7.42wolfsburg.de>
2023-03-19 18:23:30 +0100

    game controller

---
+++ server/src/game/game.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Jwt_Auth_Guard } from 'src/auth/guards/jwt_auth.guard';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { GameService } from './game.service';
+
+@Controller('game')
+export class GameController
+{
+	constructor(
+		private readonly gameService: GameService,
+	) {}
+
+
+	// @UseGuards(Jwt_Auth_Guard)
+	@Post('many_games_data')
+	async	get_may_games_data(@Body('game_ids') game_ids: number[])
+	{
+		return (this.gameService.get_many_games(game_ids));
+	}
+
+	// @UseGuards(Jwt_Auth_Guard)
+	@Get('game_data/:game_id')
+	async	get_game_data(@Param('game_id') game_id)
+	{
+		console.log(game_id);
+		console.log(Number.isNaN(Number(game_id)));
+		if (!Number.isNaN(Number(game_id)))
+		{
+			console.log("i would do it");
+			
+			// return (this.gameService.get_one_game(Number(game_id)));
+		}
+		else
+			console.log("i would not do it");
+	}
+}
--- server/src/game/game.module.ts
+++ server/src/game/game.module.ts
@@ -3,8 +3,10 @@ import { GameService } from './game.service';
 import { GameGateway } from './game.gateway';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
+import { GameController } from './game.controller';
 
 @Module({
-  providers: [GameGateway, GameService, PrismaService, UserService]
+  providers: [GameGateway, GameService, PrismaService, UserService],
+  controllers: [GameController]
 })
 export class GameModule {}

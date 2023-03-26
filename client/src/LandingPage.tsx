5af6f71b88c600ccd8264fdb2dce93bdf6ee942c
= <kuba.kaczmarski312@gmail.com>
2023-03-26 18:26:22 +0200

    new cat

--- client/src/LandingPage.tsx
+++ client/src/LandingPage.tsx
@@ -1,7 +1,47 @@
 const LandingPage = () => {
 
-	return <div>
-		Welcome to Pong
+	return (
+	<>
+	<div>
+	<div className='cat'>
+  <div className='head'>
+    <div className='face'>
+      <div className='stripes'>
+        <div className='top'></div>
+        <div className='left'></div>
+        <div className='right'></div>
+      </div>
+      <div className='eyes'>
+        <div className='left'></div>
+        <div className='right'></div>
+      </div>
+      <div className='nose'></div>
+      <div className='mouth'></div>
+    </div>
+    <div className='ears'>
+      <div className='left'></div>
+      <div className='right'></div>
+    </div>
+  </div>
+  <div className='suit'>
+    <div className='collar'>
+      <div className='top'></div>
+    </div>
+    <div className='arms'>
+      <div className='left'></div>
+      <div className='right'></div>
+    </div>
+    <div className='paws'>
+      <div className='left'></div>
+      <div className='right'></div>
+    </div>
+    <div className='body'></div>
+  </div>
+  <div className='tail'></div>
+  <div className='shadow'></div>
+</div>
+
 	</div>
+	</>)
 }
 export default LandingPage

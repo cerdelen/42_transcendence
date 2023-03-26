ee64825c3e17df01020d16c13721d6b23ee2c0fb
kmilchev <kmilchev@student.42wolfsburg.de>
2023-03-26 18:29:10 +0200

    figuring out the right panel

--- client/src/components/ChatPanel/Community_left_collumn.tsx
+++ client/src/components/ChatPanel/Community_left_collumn.tsx
@@ -6,7 +6,7 @@ import Get_all_other_users from "./All_other_users";
 
 const Community_left_collumn = ({userId, my_chats_ids, setmy_chats_ids} : { userId: string, my_chats_ids: number[], setmy_chats_ids:any}) => {
 	return (
-		<div className='Community-left-collumn'>
+		<div className='community-side-collumn'>
 			<Get_all_my_chats userId={userId} my_chats_ids={my_chats_ids} setmy_chats_ids={setmy_chats_ids}/>
 			<Get_all_other_users />
 		</div>

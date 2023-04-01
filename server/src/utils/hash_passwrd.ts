5ba635e2e79a9a02f244fb25f0521519c050f680
Ruslan Kultaev <ruslankultaev7@gmail.com>
2023-04-01 17:07:48 +0200

    pwd

---
+++ server/src/utils/hash_passwrd.ts
@@ -0,0 +1,10 @@
+import * as bcrypt from 'bcrypt';
+
+export const hashPassword = (rawPassword: string) => {
+	const salt = bcrypt.genSaltSync();
+	return bcrypt.hashSync(rawPassword, salt);
+}
+
+export const comparePassword = (rawPassword: string, hash: string) => {
+	return bcrypt.compareSync(rawPassword, hash);
+}

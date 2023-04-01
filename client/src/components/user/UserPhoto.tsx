4dea3eccb32b5ef91175617a9978430d37048659
kmilchev <kmilchev@student.42wolfsburg.de>
2023-04-01 13:45:11 +0200

    photo updates correctly in the user info card

--- client/src/components/user/UserPhoto.tsx
+++ client/src/components/user/UserPhoto.tsx
@@ -1,6 +1,4 @@
-import { useContext, useEffect, useState } from "react";
-import { UserContext } from "../../contexts/UserContext";
-import JSCookies from "js-cookie";
+import { useEffect, useState } from "react";
 import defaultPicture from "../../images/default-picture.jpeg";
 import { useMyProfile_picture_Context } from "../../contexts/Profile_picture_context";
 
@@ -8,37 +6,24 @@ interface Props {
   userId: string;
 }
 
-const UserPhoto = ({ userId }: Props) => {  
+const UserPhoto = ({ userId }: Props) => {
   const [photoURL, setPhotoURL] = useState<string>(defaultPicture);
-	const { picture_map, set_picture_map, pushPictureToMap } = useMyProfile_picture_Context();
+  const { picture_map, set_picture_map, pushPictureToMap } =
+    useMyProfile_picture_Context();
 
   useEffect(() => {
     const getUserPic = async () => {
-      if(picture_map.has(Number(userId)))
-      {
-        setPhotoURL(picture_map.get(Number(userId)) ?? '');
-        return ;
+      if (picture_map.has(Number(userId))) {
+        setPhotoURL(picture_map.get(Number(userId)) ?? "");
+        return;
       }
-      setPhotoURL(pushPictureToMap(Number(userId), picture_map, set_picture_map));
-    };
-    const displayPhoto = async () => {
-      const response = await fetch(
-        "http://localhost:3003/pictures/is-image-default",
-        {
-          method: "Get",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${JSCookies.get("accessToken")}`,
-          },
-        }
+      setPhotoURL(
+        pushPictureToMap(Number(userId), picture_map, set_picture_map)
       );
-
-      const data = await response.json();
-      if (!data["status"] && userId) getUserPic();
     };
-    displayPhoto();
+    getUserPic();
   }, [userId]);
-  return <img id="photo" src={photoURL}/>;
+  return <img id="photo" src={photoURL} />;
 };
 
 export default UserPhoto;

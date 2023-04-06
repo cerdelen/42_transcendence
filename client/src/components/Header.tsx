94bcd33dbfb17bb8f206c0f4afea00176085d9f4
Cedric Erdelen <cerdelen@c1s8d35.42wolfsburg.de>
2023-04-06 18:36:10 +0200

    fixed popups

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
   return (
     <header>
       <Link to="/">
-        <img src={logo} alt="" id="logo" />
+        <img src={logo} alt="" onClick={closePopUps}  id="logo" />
       </Link>
       <nav>
         <ul>
